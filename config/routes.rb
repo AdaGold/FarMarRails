00756b27f4d27aebae128a7ee6ea4ce042cf64ce
“BJHunnicutt” <jeannie.hunnicutt@gmail.com>
2016-10-05 10:24:47 -0700

    Added user type hompage with buttons

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'users#index'
   get 'users/index', as: 'index'
 
-  get 'users/show'
+  get 'users/show/:type/' => 'users#show', as: "show_user"
 
   get 'users/new'
 
