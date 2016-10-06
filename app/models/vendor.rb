f326a7859649213da2aa684fcbcd4a9ae71fb147
“BJHunnicutt” <jeannie.hunnicutt@gmail.com>
2016-10-06 16:38:33 -0700

    added methods to find revenue for a month

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -13,4 +13,24 @@ class Vendor < ActiveRecord::Base
     return revenue
   end
 
+  def revenue_between(beginning_time, end_time)
+    sales_between = 0
+
+    sales.each do |sale|
+      if sale.purchase_time >= beginning_time && sale.purchase_time < end_time
+        sales_between += sale.amount
+      end
+    end
+    return sales_between
+  end
+
+  def revenue_by_month(month, year)
+    beginning_time = DateTime.new(year, month)
+    end_time = DateTime.new(year, month+1)
+
+    revenue = revenue_between(beginning_time, end_time)
+
+    return revenue
+  end
+
 end
